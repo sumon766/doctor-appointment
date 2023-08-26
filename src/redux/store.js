31b7f383940e9c41bfbb7203b82c26c57f037395
Elhambasir <elham1378basir@gmail.com>
2023-08-26 20:19:10 +0430

    Add embeded code to get data from database and display it in UI

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import doctorListReducer from "./doctor_list_slice";
 import UISlice from "./UISlice";
 
 const store = configureStore({
   reducer: {
     ui: UISlice,
+    doctorList: doctorListReducer,
   },
 });
 
