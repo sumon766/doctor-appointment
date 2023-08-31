3217d2afd9b586eb422444507e60b70c548820e0
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-31 08:01:02 -0400

    Add postcss to the development

--- src/components/appointment/MyAppointment.js
+++ src/components/appointment/MyAppointment.js
@@ -1,53 +1,37 @@
 import React from 'react';
+import { list } from 'postcss';
 import s from './myappointment.module.scss';
 
 export default function MyAppointment() {
-  // const appointmentlists = [{
-  //   id: 1,
-  //   date: 'Sat, 09 Sep 2023',
-  //   city: "Addis Ababa",
-  //   doctor_id: 'Dr . Joe',
-  // },
-  // {
-  //   id: 2,
-  //   date: 'Sat, 09 Sep 2023,',
-  //   city: "Addis Ababa",
-  // },
-  // {
-  //   id: 3,
-  //   date: 'Sat, 09 Sep 2023',
-  //   city: "Nairobi",
-  //   doctor_id: 'Dr. Hope',
-  // }];
+  const appointmentlists = [{
+    id: 1,
+    date: 'Sat, 09 Sep 2023',
+    city: "Addis Ababa",
+    doctor_id: 'Dr . Joe',
+  },
+  {
+    id: 2,
+    date: 'Sat, 09 Sep 2023,',
+    city: "Addis Ababa",
+    doctor_id: 'Dr. Hope',
+  },
+  {
+    id: 3,
+    date: 'Sat, 09 Sep 2023',
+    city: "Nairobi",
+    doctor_id: 'Dr. Hope',
+  }];
   return (
     <div className={s.myappointmentContainer}>
       <div className={s.appontmentsTable}>
         <table>
-          <tr>
-            <th>Date</th>
-            <th>City</th>
-            <th>Doctor</th>
-          </tr>
-          <tr>
-            <td>Data 1A</td>
-            <td>Data 1B</td>
-            <td>Data 1C</td>
-          </tr>
-          <tr>
-            <td>Data 1A</td>
-            <td>Data 1B</td>
-            <td>Data 1C</td>
-          </tr>
-          <tr>
-            <td>Data 1A</td>
-            <td>Data 1B</td>
-            <td>Data 1C</td>
-          </tr>
-          <tr>
-            <td>Data 1A</td>
-            <td>Data 1B</td>
-            <td>Data 1C</td>
-          </tr>
+          {appointmentlists.map(list)}
+          {/* <tr>
+            <th>{list.date}</th>
+            <th>{list.city}</th>
+            <th>{list.doctor_id}</th>
+          </tr> */}
+
         </table>
       </div>
 
