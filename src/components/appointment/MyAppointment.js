fe049ab6218f91ddb4af4473a558ef4be98749a8
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-31 15:16:45 -0400

    Display appointments on the its own page

--- src/components/appointment/MyAppointment.js
+++ src/components/appointment/MyAppointment.js
@@ -1,25 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAppointment } from '../../redux/appointmentSlice';
 import s from './myappointment.module.scss';
 
 export default function MyAppointment() {
-  const appointmentlists = [{
-    id: 1,
-    date: 'Sat, 09 Sep 2023',
-    city: "Addis Ababa",
-    doctor_id: 'Dr . Joe',
-  },
-  {
-    id: 2,
-    date: 'Sat, 09 Sep 2023,',
-    city: "Addis Ababa",
-    doctor_id: 'Dr. Hope',
-  },
-  {
-    id: 3,
-    date: 'Sat, 09 Sep 2023',
-    city: "Nairobi",
-    doctor_id: 'Dr. Hope',
-  }];
+  const { appointmentlists } = useSelector(((state) => state.appointment));
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAppointment());
+    console.log(appointmentlists);
+  }, [dispatch]);
   return (
     <div className={s.myappointmentContainer}>
       <div className={s.appontmentsTable}>
