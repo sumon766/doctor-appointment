f4c5865ccfa9c648e9d544e121597fb344d839f8
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-31 07:22:31 -0400

    Add MyAppointment Component

---
+++ src/components/appointment/MyAppointment.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './myappointment.module.scss';
+
+export default function MyAppointment() {
+  return (
+    <div className={s.myappointmentContainer}>MyAppointment</div>
+  );
+}
