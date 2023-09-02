ab7108ba519f54f0508f5cce1b0aa3cd9a681a4b
Lul-Abdifan <tasanbannu@gmail.com>
2023-09-02 16:23:11 -0400

    Update Navbar style

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { FaTimes, FaBars } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarsStaggered, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import s from "./navbar.module.scss";
 
@@ -11,10 +12,14 @@ const Navbar = () => {
   const toggleNavbar = () => {
     setIsNavOpen(!isNavOpen);
   };
-  const toggleLink = () => {
-    navRef.current.classList.toggle(s.removeNavbar);
-  };
 
+  const handleLinkClick = () => {
+    navRef.current.classList.toggle(s.removeNavbar);
+    setTimeout(() => {
+      window.location.reload();
+    }, 100);
+  };
+  window.addEventListener("resize", handleLinkClick);
   const navLinks = [
     { to: "/", text: "Doctor" },
     { to: "/appointment", text: "Appointment" },
@@ -31,11 +36,12 @@ const Navbar = () => {
         onClick={toggleNavbar}
         type="button"
       >
-        {isNavOpen ? <FaTimes /> : <FaBars />}
+        {isNavOpen ? <FontAwesomeIcon icon={faTimes} />
+          : <FontAwesomeIcon icon={faBarsStaggered} className="bar-custom-icon" />}
       </button>
       <nav className={isNavOpen ? s["mobile-navbar"] : s["desktop-navbar"]}>
         {navLinks.map((link) => (
-          <div className={s["navbar-links"]} key={link.to}><Link to={link.to} onClick={toggleLink}>{link.text}</Link></div>
+          <div className={s["navbar-links"]} key={link.to}><Link to={link.to} onClick={handleLinkClick}>{link.text}</Link></div>
 
         ))}
       </nav>
