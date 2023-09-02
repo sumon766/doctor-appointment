70397418f81adf8a2cadc5fae1180c8db488386a
James Serengia <jamesserengia@gmail.com>
2023-09-02 08:15:47 +0300

    Start setting up auth

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import './login.scss';
+import React from "react";
+import "./login.scss";
+import { toast } from "react-toastify";
+// import useAxios from "../../hooks/useAxios";
 
 function Login() {
+  // const { axios } = useAxios();
+
+  const submitHandler = (e) => {
+    const username = e.target.value?.username;
+    const password = e.target.value?.password;
+    if (!username || !password) {
+      toast.error("Both username and password are required.");
+    }
+  };
   return (
     <div className="login">
       <div className="login-content">
         <h1 className="login_title">Login</h1>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className="login_form_input">
-            <input type="email" id="email" className="login-input" placeholder="Enter your username..." />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="login-input"
+              placeholder="Enter your username..."
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="login-input"
+              placeholder="..."
+            />
           </div>
           <div className="login_form_input">
-            <button type="submit" className="login-button ">Login</button>
+            <button type="submit" className="login-button ">
+              Login
+            </button>
           </div>
         </form>
       </div>
